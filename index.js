42f35e660c39ee75aea19afd3974d68b86034a33
Jun Liang <illuminatemynight@gmail.com>
2020-05-09 18:54:45 -0500

    edit index.js

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 window.addEventListener('load', () => {
     const sounds = document.querySelectorAll(".sound");
     const pads = document.querySelectorAll(".pads div");
+    const visual = document.querySelector(".visual");
+    const colors = [
+        "#60d394",
+        "#de6060",
+        "#c060d3",
+        "#d3d160",
+        "#6860d3",
+        "#0f5bbeb9"
+    ];
     
     //Sound
     pads.forEach((pad, index) => {
         pad.addEventListener("click", function(){
             sounds[index].currentTime = 0;
             sounds[index].play();
+
+            createBubbles(index);
         });
     });
+
+    //Function
+    const createBubbles = (index) => {
+        const bubble = document.createElement("div");
+        visual.appendChild(bubble);
+        bubble.style.background = colors[index];
+        bubble.style.animation = 'jump 1s ease';
+        bubble.addEventListener('animationend', function() {
+            visual.removeChild(bubble);
+        })
+    }
 });
 
